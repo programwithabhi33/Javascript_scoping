65d238c5f3f4d71d7f38ff92e819541d05fbe0d5
abhi <programwithabhi@gmail.com>
2023-06-14 20:16:34 +0530

    non exported variable on the

--- module.js
+++ module.js
@@ -1,11 +1,14 @@
 // This is the module file you can access another file variables that are not modules and if these file are modules then they must be export that variables to access in this file
 import { exportVar } from './moduleExport.js'
 
-Y/* ou can access exportVar variable because the moduleExport.js file has the module file and this file exported this variable and you imported this variable that's why you can access this variable */
+/* You can access exportVar variable because the moduleExport.js file has the module file and this file exported this variable and you imported this variable that's why you can access this variable */
 
 console.log(exportVar)
 
 /* You can access varaible a because variable is defined in the global.js file and this file is not a module if it is then you need to export a variable to access here with import */
 
 console.log(a)
+
+// You cannot access firstVar variable because moduleExport file not exported the firstVar variable
+// console.log(firstVar)
 
